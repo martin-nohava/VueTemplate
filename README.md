3dfd832a2bf8e5a0fd557057f52c4ec5140c3332
Martin Nohava <martin.nohava.mn@gmail.com>
2020-10-12 12:22:00 +0200

    End of chapter 3

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # VueTemplate
 **First web application created with Vue.js framework**
 Main goal is to try implement some basic feutures into web app via JavaScript and Vue.js
+Based on this tutorial: https://www.youtube.com/watch?v=ZqgiuPt5QZo
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    {{user.username}} - {{fullName}} 
+    <strong>Followers</strong> 
+    {{folowers}}
+    <button @click="followUser">Follow</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  //Function for storing and returning data of our application
+  data() {
+    return {
+      folowers: 0,
+      //Temp user object
+      user: {
+        id: 1,
+        username: '@martin_nohava',
+        firstName: 'Martin',
+        lastName: 'Nohava',
+        email: 'martin@nohava.cz',
+        isAdmin: true
+      }
+    }
+  },
+  //Watches a datapoint and when change is detected on that datapoint this method is executed
+  watch: {
+    folowers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower! Congrats!`);
+      }
+    }
+  },
+  //Object for computed data
+  computed: {
+    fullName() {
+      //${} is used for formating text insted of this.user.firstName + this.user.lastName
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    followUser() {
+      this.folowers++;
+    }
+  },
+  //Methot that runs when the component is loaded for the first time
+  mounted() {
+    this.followUser();
   }
 }
 </script>
@@ -19,8 +57,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 }
 </style>
