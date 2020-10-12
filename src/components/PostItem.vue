0734c2c5076a76284e03dc528a2c0ffbe9d72273
Martin Nohava <martin.nohava.mn@gmail.com>
2020-10-12 13:49:55 +0200

    End of chapter 4

---
+++ src/components/PostItem.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="post-item" @click="favouritePost(post.id)">
+        <div class="user-profile_post">
+            <div class="post-item_user">
+                {{username}}
+            </div>
+            <div class="post-item_content">
+                {{post.content}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PostItem',
+    props: {
+        username: {
+            type: String,
+            required: true
+        },
+        post: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        favouritePost(id) {
+            this.$emit('favourite', id)
+        }
+    }
+}
+</script>
+
+<style>
+.post-item {
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid whitesmoke;
+    box-sizing: border-box;
+    cursor: pointer;
+    transition: all 0.25s ease;
+}
+
+.post-item:hover {
+    transform: scale(1.1, 1.1);
+}
+
+.post-item_user {
+    font-weight: bold;
+}
+
+</style>
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -2,16 +2,33 @@
   <div class="user-profile">
     <div class="user-profile_user-panel">
         <h1 class="user-profile_username">{{user.username}}</h1>
+        <div class="user-profile_admin-badge" v-if="user.isAdmin">
+            Admin
+        </div>
         <div class="user-profile_follower-count">
             <strong>Followers</strong> {{folowers}}
         </div>
+    </div>
+    <div class="user-profile_posts-wrapper">
+       <PostItem 
+            v-for="post in user.posts" 
+            v-bind:key="post.id" 
+            v-bind:username="user.username" 
+            v-bind:post="post" 
+            @favourite="toggleFavourite">
+       </PostItem>
     </div>
   </div>
 </template>
 
 <script>
+import PostItem from './PostItem.vue';
+
 export default {
   name: 'UserProfile',
+  components: {
+      PostItem
+  },
   //Function for storing and returning data of our application
   data() {
     return {
@@ -23,7 +40,17 @@ export default {
         firstName: 'Martin',
         lastName: 'Nohava',
         email: 'martin@nohava.cz',
-        isAdmin: true
+        isAdmin: true,
+        posts: [
+            {
+                id: 1,
+                content: 'Hello World, my first post!'
+            },
+            {
+                id: 2,
+                content: 'Catch me if you can.'
+            }
+        ]
       }
     }
   },
@@ -45,6 +72,9 @@ export default {
   methods: {
     followUser() {
       this.folowers++;
+    },
+    toggleFavourite(id) {
+        console.log('Favorited post ' + id)
     }
   },
   //Methot that runs when the component is loaded for the first time
@@ -70,6 +100,15 @@ export default {
     background-color: white;
     border-radius: 5px;
     border: 1px solid whitesmoke;
+}
+
+.user-profile_admin-badge {
+    background: navy;
+    color: white;
+    border-radius: 5px;
+    margin-right: auto;
+    padding: 0 10px;
+    font-weight: bold;
 }
 
 h1 {
